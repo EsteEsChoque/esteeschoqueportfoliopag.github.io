a9af1723e4d7fcacef662203fa7203bdff17bf6e
EsteEsChoque <86280354+EsteEsChoque@users.noreply.github.com>
2023-10-04 18:39:03 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function seleccionar(link){
+ 
+    var links = document.getElementById("links").getElementsByTagName("li");
+ 
+    links[0].children[0].className="";
+    links[1].children[0].className="";
+    links[2].children[0].className="";
+    links[3].children[0].className="";
+    links[4].children[0].className="";
+    link.className="seleccionado";
+
+    var x= document.getElementById("nav");
+    x.className="";
+}
+
+
+function responsiveMenu(){
+    var x = document.getElementById("nav");
+    if(x.className===""){
+        x.className = "responsive";
+    }
+    else{
+        x.className = "";
+    }
+}
